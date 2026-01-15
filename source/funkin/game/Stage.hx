bb356bde38bf44c95207e2ff64825fed3ad53921
StarNova <creambr6@gmail.com>
2026-01-15 08:47:14 -0300

    Update Stage.hx

--- source/funkin/game/Stage.hx
+++ source/funkin/game/Stage.hx
@@ -90,7 +90,7 @@ class Stage extends FlxBasic implements IBeatReceiver {
 			stageName = xml.getAtt("name").getDefault(stageFile);
 
 			if (PlayState.instance == state) {
-				if(xml.has.startCamPosX) PlayState.instance.camFollow.x = startCam.x;
+				if(xml.has.startCamPosX) PlayState.instancedestroystroyageScriptageScriptamFollow.x = startCam.x;
 				if(xml.has.startCamPosY) PlayState.instance.camFollow.y = startCam.y;
 				if(xml.has.zoom) PlayState.instance.defaultCamZoom = defaultZoom;
 			}
@@ -370,7 +370,7 @@ class Stage extends FlxBasic implements IBeatReceiver {
 
 	public override function destroy() {
 		if (PlayState.instance == state && PlayState.instance.scripts != null) PlayState.instance.gameAndCharsCall("onStageDestroy", [this]);
-		stageScript?.call("destroy");
+		//stageScript?.call("destroy");
 		destroySilently();
 	}
 
